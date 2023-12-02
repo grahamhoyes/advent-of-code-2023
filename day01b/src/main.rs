ea2c0bdc5c7d61c5181ad17a5f6bd72e01c85ed7
Graham Hoyes <accounts@grahamhoyes.com>
2023-12-02 12:26:15 -0500

    More refactoring

--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -1,13 +1,13 @@
-const REPLACEMENTS: &[(&'static str, &'static str)] = &[
-    ("one", "1"),
-    ("two", "2"),
-    ("three", "3"),
-    ("four", "4"),
-    ("five", "5"),
-    ("six", "6"),
-    ("seven", "7"),
-    ("eight", "8"),
-    ("nine", "9"),
+const REPLACEMENTS: &[(&str, u32)] = &[
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
 ];
 
 fn solution(input: &str) -> u32 {
@@ -17,44 +17,43 @@ fn solution(input: &str) -> u32 {
             // Need to account for things like "eightwo", which should be considered "82"
 
             // Tuple is (position, value)
-            let mut numbers: Vec<(usize, String)> = vec![];
+            let mut numbers: Vec<(usize, u32)> = vec![];
 
-            for r in REPLACEMENTS {
+            for (word, val) in REPLACEMENTS {
                 // Find the first and last spelled out digits, if there are any
-                if let Some(idx) = l.find(r.0) {
-                    numbers.push((idx, r.1.into()));
+                if let Some(idx) = l.find(word) {
+                    numbers.push((idx, *val));
                 }
 
-                if let Some(idx) = l.rfind(r.0) {
-                    numbers.push((idx, r.1.into()));
+                if let Some(idx) = l.rfind(word) {
+                    numbers.push((idx, *val));
                 }
             }
 
             // Find regular digits
             if let Some(idx) = l.find(char::is_numeric) {
-                let num = l.chars().nth(idx).unwrap().to_string();
+                let num = l.chars().nth(idx).unwrap().to_digit(10).unwrap();
                 numbers.push((idx, num));
             }
 
             if let Some(idx) = l.rfind(char::is_numeric) {
-                let num = l.chars().nth(idx).unwrap().to_string();
+                let num = l.chars().nth(idx).unwrap().to_digit(10).unwrap();
                 numbers.push((idx, num));
             }
 
             // Sort the numbers by index
             numbers.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
 
-            let first_num = numbers.first().unwrap().clone().1;
-            let last_num = numbers.last().unwrap().clone().1;
+            let first_num = numbers.first().unwrap().1;
+            let last_num = numbers.last().unwrap().1;
 
-            format!("{}{}", first_num, last_num).parse::<u32>().unwrap()
+            first_num * 10 + last_num
         })
         .sum()
 }
 
 fn main() {
     let input = include_str!("../input.txt");
-
     let res = solution(input);
 
     println!("Result: {}", res);
--- day02a/src/main.rs
+++ day02a/src/main.rs
@@ -8,30 +8,31 @@ struct Draw {
 }
 
 fn solution(input: &str) -> usize {
-    let mut games: HashMap<usize, Vec<Draw>> = HashMap::new();
-
     let red_re = Regex::new(r"(\d+) red").unwrap();
     let green_re = Regex::new(r"(\d+) green").unwrap();
     let blue_re = Regex::new(r"(\d+) blue").unwrap();
 
-    for l in input.lines() {
-        let parts: Vec<&str> = l.split(':').collect();
-        let game_id: usize = parts[0][5..].parse().unwrap();
+    let games: HashMap<usize, Vec<Draw>> = input
+        .lines()
+        .map(|l| {
+            let parts: Vec<&str> = l.split(':').collect();
+            let game_id: usize = parts[0][5..].parse().unwrap();
 
-        let draws: Vec<Draw> = parts[1]
-            .trim()
-            .split(';')
-            .map(|text| {
-                let r = red_re.captures(text).map_or(0, |c| c[1].parse().unwrap());
-                let g = green_re.captures(text).map_or(0, |c| c[1].parse().unwrap());
-                let b = blue_re.captures(text).map_or(0, |c| c[1].parse().unwrap());
+            let draws: Vec<Draw> = parts[1]
+                .trim()
+                .split(';')
+                .map(|text| {
+                    let r = red_re.captures(text).map_or(0, |c| c[1].parse().unwrap());
+                    let g = green_re.captures(text).map_or(0, |c| c[1].parse().unwrap());
+                    let b = blue_re.captures(text).map_or(0, |c| c[1].parse().unwrap());
 
-                Draw { r, g, b }
-            })
-            .collect();
+                    Draw { r, g, b }
+                })
+                .collect();
 
-        games.insert(game_id, draws);
-    }
+            (game_id, draws)
+        })
+        .collect();
 
     // Possible games with 12 red, 13 green, and 14 blue
     let possible_games = games.iter().filter_map(|(k, draws)| {
