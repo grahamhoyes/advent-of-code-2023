1038fb5fe21fb33974b0e6c2bc2d53421f2b50e9
Graham Hoyes <accounts@grahamhoyes.com>
2023-12-17 12:38:43 -0500

    Day 17 part 1

--- day17a/src/main.rs
+++ day17a/src/main.rs
@@ -1,24 +1,16 @@
-// We need to implement Dijkstra's algorithm, with the caveat that we can't
-// move straight more than 3 times. In other words, the cost of moving straight
-// after already having moved straight thrice is infinite.
-//
-// I don't see a way to add that condition into the interfaces exposed
-// by the `pathfinding` crate (since the neighbors function doesn't get given the
-// path so far), so we do it ourselves.
-//
-// Note that the property of not re-visiting a node is maintained. If we were to
-// cross a straight path, we could have made a turn there originally with less cost.
-// Performing a loop to cross an angled path is also longer than pathing to the next
-// node directly.
-
 use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashMap, HashSet};
-use std::ops::Add;
 
 /// A (row, col) coordinate pair or vector. Using i32 so that we can subtract
 /// when needed, but only positive values are valid.
 #[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
 struct Coord(i32, i32);
+
+impl From<Coord> for (i32, i32) {
+    fn from(value: Coord) -> Self {
+        (value.0, value.1)
+    }
+}
 
 #[derive(Hash, Clone, Copy, Debug, Eq, PartialEq)]
 enum Dir {
@@ -28,29 +20,6 @@ enum Dir {
     West,
 }
 
-const DIRECTIONS: [Dir; 4] = [Dir::North, Dir::East, Dir::South, Dir::West];
-
-impl Add<&Dir> for &Coord {
-    type Output = Coord;
-
-    fn add(self, rhs: &Dir) -> Self::Output {
-        match rhs {
-            Dir::North => Coord(self.0 - 1, self.1),
-            Dir::East => Coord(self.0, self.1 + 1),
-            Dir::South => Coord(self.0 + 1, self.1),
-            Dir::West => Coord(self.0, self.1 - 1),
-        }
-    }
-}
-
-impl Add<&Dir> for Coord {
-    type Output = Self;
-
-    #[allow(clippy::op_ref)]
-    fn add(self, rhs: &Dir) -> Self::Output {
-        &self + rhs
-    }
-}
 struct Board {
     inner: Vec<Vec<usize>>,
 }
@@ -86,45 +55,20 @@ impl Board {
             .and_then(|row| row.get(c.1 as usize))
             .cloned()
     }
-
-    /// Get the valid neighboring edge connections of a coordinate
-    fn neighbors(&self, from: &Coord) -> Vec<Edge> {
-        DIRECTIONS
-            .iter()
-            .filter_map(|dir| {
-                let target = from + dir;
-                let next = self.get(&target)?;
-
-                Some(Edge {
-                    target,
-                    direction: *dir,
-                    cost: next,
-                })
-            })
-            .collect()
-    }
-}
-
-#[derive(Eq, PartialEq)]
-struct Edge {
-    /// Coordinate of the target node
-    target: Coord,
-    /// Direction travelled from the previous node
-    direction: Dir,
-    /// Cost incurred by this transition
-    cost: usize,
 }
 
 #[derive(Debug, Eq, PartialEq)]
 struct Visit {
-    vertex: Coord,
+    coord: Coord,
     /// Total heat lost travelling from the start through this vertex
     heat_lost: usize,
+    /// The direction we came from
+    direction: Dir,
 }
 
 // The binary heap we use to track edges depends on `Ord`. Explicitly implementing
 // this trait with the comparison backwards so that we get a min-heap, rather than
-// a max-heap.
+// a max-heap. The only thing we order on is heat lost (the cost).
 impl Ord for Visit {
     fn cmp(&self, other: &Self) -> Ordering {
         other.heat_lost.cmp(&self.heat_lost)
@@ -138,52 +82,84 @@ impl PartialOrd for Visit {
 }
 
 fn solution(input: &str) -> usize {
-    let mut board = Board::from_input(input);
+    let board = Board::from_input(input);
 
-    // This implementation of Dijkstra's algorithm is mostly from
-    // https://codereview.stackexchange.com/a/202879
-    let mut costs: HashMap<Coord, usize> = HashMap::new();
-    let mut visited: HashSet<Coord> = HashSet::new();
+    let (rows, cols) = board.size();
+    let goal = Coord(rows as i32 - 1, cols as i32 - 1);
+
+    let mut costs: HashMap<(Coord, Dir), usize> = HashMap::new();
+    let mut visited: HashSet<(Coord, Dir)> = HashSet::new();
     let mut to_visit = BinaryHeap::new();
 
-    costs.insert(Coord(0, 0), 0);
-    to_visit.push(Visit {
-        vertex: Coord(0, 0),
-        heat_lost: 0,
-    });
+    // We could travel either direction out of the starting cell
+    for dir in [Dir::South, Dir::East] {
+        costs.insert((Coord(0, 0), dir), 0);
+        to_visit.push(Visit {
+            coord: Coord(0, 0),
+            heat_lost: 0,
+            direction: dir,
+        });
+    }
 
-    while let Some(Visit { vertex, heat_lost }) = to_visit.pop() {
-        if !visited.insert(vertex) {
+    while let Some(Visit {
+        coord,
+        heat_lost,
+        direction,
+    }) = to_visit.pop()
+    {
+        if !visited.insert((coord, direction)) {
             // Already been here
             continue;
         }
 
-        for neighbor in board.neighbors(&vertex) {
-            let new_cost = heat_lost + neighbor.cost;
+        if coord == goal {
+            return heat_lost;
+        }
 
-            // TODO: If direction results in us going the same way 3 times, abort
+        let (y, x) = coord.into();
 
-            let is_cheaper = costs
-                .get(&neighbor.target)
-                .map_or(true, |&current| new_cost < current);
+        // This prevents us from going in the same direction or backwards
+        for new_direction in match direction {
+            Dir::North | Dir::South => [Dir::East, Dir::West],
+            Dir::East | Dir::West => [Dir::North, Dir::South],
+        } {
+            let mut new_cost = heat_lost;
 
-            if is_cheaper {
-                costs.insert(neighbor.target, new_cost);
-                to_visit.push(Visit {
-                    vertex: neighbor.target,
-                    heat_lost: new_cost,
-                });
+            // Rather than letting us travel straight as a separate iteration,
+            // enqueue all of the legal straight moves after going in new_direction at once.
+            for steps in 1..=3 {
+                let c = match new_direction {
+                    Dir::North => Coord(y - steps, x),
+                    Dir::East => Coord(y, x + steps),
+                    Dir::South => Coord(y + steps, x),
+                    Dir::West => Coord(y, x - steps),
+                };
+
+                if let Some(extra_cost) = board.get(&c) {
+                    new_cost += extra_cost;
+
+                    let is_cheaper = costs
+                        .get(&(c, new_direction))
+                        .map_or(true, |&current| new_cost < current);
+
+                    if is_cheaper {
+                        costs.insert((c, new_direction), new_cost);
+                        to_visit.push(Visit {
+                            coord: c,
+                            heat_lost: new_cost,
+                            direction: new_direction,
+                        })
+                    }
+                }
             }
         }
     }
 
-    let (rows, cols) = board.size();
-
-    costs[&Coord(rows as i32 - 1, cols as i32 - 1)]
+    panic!("Could not find target node")
 }
 
 fn main() {
-    let input = include_str!("../example.txt");
+    let input = include_str!("../input.txt");
     let res = solution(input);
 
     println!("Result: {}", res);
@@ -198,8 +174,7 @@ mod tests {
         let input = include_str!("../example.txt");
         let res = solution(input);
 
-        // This is what's returned by regular Dijkstra's as a reference point
-        assert_eq!(res, 78);
+        assert_eq!(res, 102);
     }
 
     #[test]
@@ -207,6 +182,6 @@ mod tests {
         let input = include_str!("../input.txt");
         let res = solution(input);
 
-        assert_eq!(res, 0);
+        assert_eq!(res, 1128);
     }
 }
